274f8024b7ca7d27917baf27d9021437dadb53b0
raywu0123 <b05901189@ntu.edu.tw>
2017-03-03 22:23:10 +0800

    20170303  2223

--- hw1/hw1_withoutregularize.py
+++ hw1/hw1_withoutregularize.py
@@ -1,6 +1,7 @@
 # coding = Big5
 __author__ = 'ray'
-
+##using gradient descent
+##only first order terms
 
 import numpy as np
 import csv
@@ -15,50 +16,59 @@ def read_file(file_name):
         B=np.array(Bp)
     return B
 
-###reading from training file, store in train_data, shape of 18*5760
+###reading from training file, store in train_data, shape of 17*5760
 A=read_file('train.csv')
-train_data=np.array([])
-train_data.resize((18,24*240))
-for i in range(240):
-    for j in range(18):
-        if j==10:
-            pass
-        else:
-            for k in range(24):
-                train_data[j][k+i*24]=float(A[1+18*i+j][3+k])
+
+def extract_data(A,hour,bias_x,bias_y):
+    data=np.array([])
+    data.resize((17,hour*240))
+    for i in range(240):
+        jp=0
+        for j in range(18):
+            if j==10:
+                pass
+            else:
+                for k in range(hour):
+                    data[jp][k+i*hour]=float(A[bias_y+18*i+j][bias_x+k])
+                jp+=1
+    return data
+train_data=extract_data(A,24,3,1)
 
 
-trails=24*240-10  ##number of data points, ie frame shifts
 ##forming data point array
-train_data_points=np.array([[0.0]])
-train_data_points.resize((24*240-10,9*18+1)) ##shape of 5750*163
+def data_to_point(data,trails,diff_m):
+    data_points=np.array([[0.0]])
+    data_points.resize((trails,9*17+1)) ##shape of 5750*154
+    for start_point in range(trails):
+        data_part=data[:,0+start_point*diff_m:9+start_point*diff_m].flatten()
+        data_part.resize((17*9+1))
+        data_part[-1]=1.0
+        data_points[start_point]=np.copy(data_part)
+    return data_points
+
 y_hat=np.array([[0.0]])
 y_hat.resize((24*240-10,1))##label of training_data
 
-for start_point in range(trails):
-    train_data_part=train_data[:,0+start_point:9+start_point].flatten()
-    train_data_part.resize((18*9+1))
-    train_data_part[-1]=1.0
-    train_data_points[start_point]=np.copy(train_data_part)
-
+train_data_points=data_to_point(train_data,24*240-10,1)
+print(train_data_points)
+for start_point in range(24*240-10):
     y_hat[start_point]=train_data[9][9+start_point]
 
 r_learn=5.5e-10
 ##training step
 theta=np.array([[0.0]])##9 hours*18 featurs + 1bias
-theta.resize((9*18+1,1))
+theta.resize((9*17+1,1))
 theta_new=np.copy(theta)
 training_it=0
 error_sum_previous=0.0
-while(time.clock()<=60*10):
+training_time=float(input('training_time='))
+while(time.clock()<=training_time):
     h_theta=np.dot(train_data_points,theta)
     error=y_hat-h_theta
     theta+=np.dot(train_data_points.T,error) * r_learn
     error_sum=np.sum(error**2)**0.5
     if training_it%1000==0: print("time= ",time.clock()," , ",error_sum)
     training_it+=1
-
-
     if error_sum_previous-error_sum<=1e-2 and error_sum_previous>error_sum:
         r_learn*=1.1
         #print('raise')
@@ -71,24 +81,8 @@ while(time.clock()<=60*10):
 
 ###reading from test file, store in test_data
 B=read_file('test_X.csv')
-test_data=np.array([])
-test_data.resize((18,9*240))
-for i in range(240):
-    for j in range(18):
-        if j==10:
-            pass
-        else:
-            for k in range(9):
-                test_data[j][k+i*9]=float(B[18*i+j][2+k])
-
-test_data_points=np.array([[0.0]])
-test_data_points.resize((240,9*18+1)) ##shape of 5750*163
-for start_point in range(240):
-    test_data_part=test_data[:,0+start_point*9:9+start_point*9].flatten()
-    test_data_part.resize((18*9+1))
-    test_data_part[-1]=1.0
-    test_data_points[start_point]=np.copy(test_data_part)
-#print(test_data_points.shape)
+test_data=extract_data(B,9,2,0)
+test_data_points=data_to_point(test_data,240,9)
 h_theta=np.dot(test_data_points,theta)
 
 with open('submission.csv', 'w') as csvfile:
